bed8f6cfa5cd6720f3ac47704e5217539ed5352f
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-04-12 13:10:10 +0300

    Lesson 3

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void successfulLoginTest() {
+        //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up ");
+    }
+}
